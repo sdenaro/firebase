ccf2520385911169968102946f1516d9d755add8
sal <sdenaro@gmail.com>
2026-04-19 23:17:52 -0400

    Add firebase-config.js with configuration details

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR AUTH DOMAIN ",
+  projectId: "PROJECT ID",
+  storageBucket: "BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
